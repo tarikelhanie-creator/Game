a0da9495d853d76f38e017298cec94b5c61d8992
unknown <tarikwin.577@gmail.com>
2026-01-04 22:40:22 +0100

    Fix deploy permissions for GitHub Actions fr this time

--- src/components/H/Hero/Hero.jsx
+++ src/components/H/Hero/Hero.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Link ,Routes} from 'react-router-dom';
 export default function Homepage() {
   return(
      <div className="flex justify-center items-center flex-col gap-2 pt-12">
-      <h1 data-testid="app-title" className="text-3xl font-bold">World cup mini-game wow!</h1>
+      <h1 data-testid="app-title" className="text-3xl font-bold">World cup mini-game!</h1>
       <p className="pt-0 mt-0">Welcome to our mini-game!</p>
       <Link to="/teams"><button className="px-12 py-6 text-4xl font-extrabold uppercase text-white bg-green-500 rounded-full border-4 border-green-800 shadow-[0_10px_0_#166534] hover:bg-green-400 active:translate-y-2 active:shadow-none transition-all duration-150">START THE GAME</button>
 </Link>
